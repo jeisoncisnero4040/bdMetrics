c797d73541e9b9df83f94d38256f24ac0a95c63f
jeisoncinsero4040 <legoladhojavergmail.com>
2025-11-27 11:31:28 -0500

    update

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ def getmetrics():
     return Response(data, mimetype="text/plain")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000,debug=True, use_reloader=False)
